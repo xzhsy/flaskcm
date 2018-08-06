4899e66182551bdb7f6ac66126d973d5a5f68add
xzhsy <xiangzhsy@163.com>
2018-08-06 12:51:17 +0800

    Update runserver.py

--- runserver.py
+++ runserver.py
@@ -23,7 +23,7 @@ if __name__ == '__main__':
     try:
         PORT = int(environ.get('SERVER_PORT', '5555'))
     except ValueError:
-        PORT = 5555
+        PORT = 8080
     app.run(HOST, PORT)
 
 
